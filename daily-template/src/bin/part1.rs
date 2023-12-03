30cfc6c87cc064ac6a471f83aa09dba508d24306
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2023-12-03 10:35:54 +0100

    feat: add new daily-template to setup new day with cargo generate

---
+++ daily-template/src/bin/part1.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("../input.txt").trim();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../test_input.txt").trim();
+        
+    }
+}
